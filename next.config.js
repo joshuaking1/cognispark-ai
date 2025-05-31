1c6cfcffeddc4a3c86c091a8c517a1c4d3a53936
Joshua Segu <segujoshua30@gmail.com>
2025-05-31 18:46:20 +0000

    Add webpack configuration to exclude Supabase Edge Functions from bundling and TypeScript checking; include ignore-loader dependency in package.json.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    // Exclude Supabase Edge Functions from webpack bundling
+    config.module.rules.push({
+      test: /supabase\/functions\/.*\.ts$/,
+      loader: 'ignore-loader',
+    });
+
+    return config;
+  },
+  // Exclude Supabase Edge Functions from TypeScript checking
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
